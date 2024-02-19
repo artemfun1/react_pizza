83b05bc8941cb39c32a6cab19804e0e56f87bc24
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-02-19 20:17:19 +0500

    tranfer cra ts

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { store } from './redux/store'
 
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement) 
 root.render(
 	// <React.StrictMode>
 
